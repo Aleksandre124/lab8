6807af52329c697fd62b07aca8c40be22a0d5572
ako12341 <94743442+ako12341@users.noreply.github.com>
2022-12-09 22:03:47 +0400

    lab8

--- lab8/src/app/laptop-managment/laptop-managment.component.ts
+++ lab8/src/app/laptop-managment/laptop-managment.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Laptop } from '../laptop.service';
+import { BehaviorSubject, Observable, } from 'rxjs';
+import { Laptop, LaptopService } from '../laptop.service';
+import { debounceTime, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-laptop-managment',
@@ -11,16 +13,75 @@ export class LaptopManagmentComponent implements OnInit {
 
   public laptops: Laptop[] = [];
 
+  public editingLaptop?: Laptop;
+
+  addForm = new FormGroup({
+    modelName: new FormControl('', Validators.required),
+    price: new FormControl('', Validators.required)
+  })
+
   editForm = new FormGroup({
     modelName: new FormControl('', Validators.required),
     price: new FormControl('', Validators.required)
   });
-  //sadfsadsadf
+
+  searchField = new FormControl('');
+  searchText$ = new BehaviorSubject('');
+  searchResults$: Observable<Laptop[]>;
+
+  addLaptop(){
+    this.laptopService.addLaptop({
+      id: '',
+      modelName: this.addForm.value.modelName,
+      price: this.addForm.value.price
+    }).subscribe(() => {
+      this.loadLaptops();
+    })
+  }
+
+  editLaptop(laptop: Laptop){
+    this.editingLaptop = laptop;
+  }
+
+  updateLaptop(){
+    this.laptopService.updateLaptop({
+      id: this.editingLaptop!.id,
+      modelName: this.editForm.value.modelName,
+      price: this.editForm.value.price
+    }).subscribe(d => {
+      this.editingLaptop = undefined;
+      this.loadLaptops();
+    })
+  }
+
+  deleteLaptop(id: string){
+    this.laptopService.deleteLaptop(id).subscribe(() =>{
+      this.loadLaptops();
+    })
+  }
+
+  cancelEditLaptop(){
+    this.editingLaptop = undefined;
+  }
+  
+  loadLaptops(){
+    this.laptopService.getLaptops().subscribe(d=>{
+      this.laptops = d;
+    })
+  }
 
 
-  constructor() { }
+  constructor(private laptopService: LaptopService) { }
 
   ngOnInit(): void {
+    this.loadLaptops();
+
+    this.searchResults$ = this.searchText$.pipe(
+      debounceTime(500),
+      switchMap(query => this.laptopService.findLaptopByName(query))
+    );
+
+    this.searchField.valueChanges.subscribe(this.searchText$);
   }
 
 }
--- lab8/src/app/laptop.service.ts
+++ lab8/src/app/laptop.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { v4 as uuidv4 } from 'uuid';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
@@ -9,6 +10,10 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 export class LaptopService {
 
   private url = 'http://localhost:3000/laptops';
+
+  public getLaptops(): Observable<Laptop[]>{
+    return this.http.get<Laptop[]>(this.url);
+  }
 
   public addLaptop(laptop: Laptop){
     laptop.id = uuidv4();
@@ -21,6 +26,12 @@ export class LaptopService {
 
   public deleteLaptop(id: string) {
     return this.http.delete(`${this.url}/${id}`);
+  }
+
+  public findLaptopByName(modelName: string) {
+    return this.http.get<Laptop[]>(this.url, {
+      params: new HttpParams().set('modelName_like', modelName)
+    });
   }
 
   constructor(private http: HttpClient) { }
